4e701ff5d927f9a4ddbd17b0ea3fb776a4ee39b4
Denilson Telaroli <ddts80@gmail.com>
2014-06-21 23:55:39 -0300

    Refactoring

--- src/main/java/br/com/flexait/crud/controller/LanguageController.java
+++ src/main/java/br/com/flexait/crud/controller/LanguageController.java
@@ -3,37 +3,47 @@ package br.com.flexait.crud.controller;
 import static br.com.caelum.vraptor.view.Results.http;
 import static br.com.caelum.vraptor.view.Results.referer;
 
+import java.util.ResourceBundle;
+
 import javax.inject.Inject;
 import javax.servlet.http.Cookie;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
-import br.com.caelum.vraptor.http.MutableRequest;
 import br.com.caelum.vraptor.http.MutableResponse;
+import br.com.flexait.crud.interceptor.LocaleInterceptor;
 
 @Controller
 public class LanguageController {
 
+	private static final String LANGUAGE_CONFIG_SUCCESS = "language.config.success";
 	@Inject private Result result;
 	@Inject private MutableResponse response;
-	@Inject private MutableRequest request;
+	@Inject private ResourceBundle resourceBundle;
 	
 	public LanguageController() {
 	}
 	
 	@Path("/{languageTag}")
 	public void set(String languageTag) {
-		Cookie cookie = new Cookie("vraptor-locale", languageTag);
-		cookie.setPath("/");
-		response.addCookie(cookie);
-		request.setParameter("tenant-name", languageTag);
-		
+		setCookie(languageTag);
+		redirect();
+	}
+
+	private void redirect() {
 		try {
 			result.use(referer()).redirect();
 		} catch (Exception e) {
-			result.use(http()).body("Successfully configured language");
+			result.use(http()).body(resourceBundle.getString(LANGUAGE_CONFIG_SUCCESS));
 		}
+	}
+
+	private void setCookie(String languageTag) {
+		Cookie cookie = new Cookie(LocaleInterceptor.VRAPTOR_LOCALE, languageTag);
+		cookie.setPath("/");
+		cookie.setMaxAge(LocaleInterceptor.MONTH);
+		response.addCookie(cookie);
 	}
 	
 }
--- src/main/java/br/com/flexait/crud/interceptor/LocaleInterceptor.java
+++ src/main/java/br/com/flexait/crud/interceptor/LocaleInterceptor.java
@@ -1,22 +1,25 @@
 package br.com.flexait.crud.interceptor;
 
+import static com.google.common.base.Objects.firstNonNull;
+
 import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.jstl.core.Config;
 
 import br.com.caelum.vraptor.BeforeCall;
 import br.com.caelum.vraptor.Intercepts;
 import br.com.caelum.vraptor.http.MutableRequest;
 
-import com.google.common.base.Objects;
-
 @Intercepts
 @RequestScoped
 public class LocaleInterceptor {
 
+	public static final String VRAPTOR_LOCALE = "vraptor-locale";
+	public static final int MONTH = 60 * 60 * 24 * 30;
 	@Inject private MutableRequest request;
 	@Inject private HttpServletResponse response;
 	
@@ -28,16 +31,26 @@ public class LocaleInterceptor {
 		Cookie[] cookies = request.getCookies();
 		if(cookies != null) {
 			for (Cookie c : cookies) {
-				if(c.getName().equals("vraptor-locale")) {
+				if(c.getName().equals(VRAPTOR_LOCALE)) {
 					Locale.setDefault(Locale.forLanguageTag(c.getValue()));
 					return;
 				}
 			}
 		}
-		String parameter = Objects.firstNonNull(request.getServletContext().getInitParameter("javax.servlet.jsp.jstl.fmt.locale"), "en-US");
-		Cookie cookie = new Cookie("vraptor-locale", parameter);
+		
+		setCookie();
+	}
+
+	private void setCookie() {
+		String parameter = firstNonNull(getInitLocale(), "en-US");
+		Cookie cookie = new Cookie(VRAPTOR_LOCALE, parameter);
 		cookie.setPath("/");
+		cookie.setMaxAge(MONTH);
 		response.addCookie(cookie);
+	}
+
+	private String getInitLocale() {
+		return request.getServletContext().getInitParameter(Config.FMT_LOCALE);
 	}
 	
 }
